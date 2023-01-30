71b71d4d1d9243b355c170379c33d321d1992e74
Amir Boufaden <amir.boufaden@scandiweb.com>
2023-01-30 18:13:43 +0100

    Initial commit

---
+++ requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/sidebar': {
+                'Magento_Checkout/js/minicart-mixin': true
+            }
+        }
+    }
+};
